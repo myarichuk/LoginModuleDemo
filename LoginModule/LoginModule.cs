f70b3c52030977b96265e7ecced91709c0291ef3
Michael Yarichuk <michael.yarichuk@gmail.com>
2019-05-15 16:03:53 +0300

    Add project files.

---
+++ LoginModule/LoginModule.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using LoginModule.Model;
+using Raven.Client.Documents;
+// ReSharper disable UnusedMember.Global
+// ReSharper disable NotAccessedField.Local
+
+namespace LoginModule
+{
+    public class LoginModule
+    {
+        private readonly IDocumentStore _store;
+        private readonly string _database;
+
+        public LoginModule(IDocumentStore store, string database)
+        {
+            _store = store;
+            _database = database;
+        }
+
+        public bool TryLogin(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RegisterUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetUserByUsername(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ValidateUserEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LoginModule/Model/User.cs
@@ -0,0 +1,17 @@
+﻿// ReSharper disable UnusedMember.Global
+namespace LoginModule.Model
+{
+    public class User
+    {
+        public string Firstname { get; set; }
+
+        public string Lastname { get; set; }
+
+        public string Email { get; set; }
+
+        public bool IsValidated { get; set; }
+
+        public string HashedPassword { get; set; }
+    }
+}
+
